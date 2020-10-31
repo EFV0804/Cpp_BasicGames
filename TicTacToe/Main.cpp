bd889c5b71f64d87bbf205762524e307ded6237a
EFV0804 <evidal@artfx.fr>
2020-10-31 21:52:53 +0100

    isSucces ok, isTie WIP

--- TicTacToe/Main.cpp
+++ TicTacToe/Main.cpp
@@ -14,31 +14,15 @@ using std::srand;
 void instructions();
 void boardDisplay(array<char, 9> boardValues);
 array<char, 9> boardUpdate(int playerTurn, int playerInput, array<char, 9>& boardValues);
-//bool isSuccessPlayerO(bool* pSuccess, char* pWinCons, char playerO);
-//bool isSuccessPlayerX(bool* pSuccess, char* pWinCons, char playerX);
+bool isSuccessPlayerO(bool* pSuccess, array<char, 9> boardValues, char playerO);
+bool isSuccessPlayerX(bool* pSuccess, array<char, 9> boardValues, char playerX);
+
 
 int main(int argc, char** argv)
 {
 	// INITIAL BOARD VALUES + POINTER
 
 	array<char, 9> boardValues = { '0', '1', '2', '3', '4', '5', '6', '7', '8' };
-	//char* pBoardValues;
-	//pBoardValues = &boardValues[0];
-
-	//WINCON ARRAY + POINTER
-	
-	array<array<char, 3>, 8> winCons =
-	{ {
-	{ boardValues[0], boardValues[1], boardValues[2] },
-	{ boardValues[0], boardValues[3], boardValues[6] },
-	{ boardValues[0], boardValues[4], boardValues[8] },
-	{ boardValues[1], boardValues[4], boardValues[7] },
-	{ boardValues[3], boardValues[4], boardValues[5] },
-	{ boardValues[2], boardValues[4], boardValues[6] },
-	{ boardValues[2], boardValues[5], boardValues[8] },
-	{ boardValues[6], boardValues[7], boardValues[8] }
-	} };
-
 
 	// PLAYER SYMBOLS
 
@@ -55,6 +39,7 @@ int main(int argc, char** argv)
 	pPlayerTurn = &playerTurn;
 
 	cout << "The starting player is Player " << playerTurn <<endl;
+	boardDisplay(boardValues);
 
 	// GAME LOOP
 	int playerInput;
@@ -70,52 +55,53 @@ int main(int argc, char** argv)
 	int a = 0;
 	int i = 0;
 	int counter = 0;
+	bool tie = false;
+	bool* pTie = &tie;
 
-	while (success == false /*|| quit == false*/)
+	while (*pSuccess == false /*|| *pTie == false*/)
 	{
-		boardDisplay(boardValues);
-
-
+		
 		if (playerTurn == 1 )
 		{
 			cout << "Player 1, choose a spot to mark: " << endl;
 			cin >> playerInput;
-
-			if (boardValues[playerInput] == playerO)
+			while ( playerInput > 8)
+			{
+				cout << "Invalid input. Pick a number between 0 and 8" << endl;
+				cin >> playerInput;
+			}
+			while (boardValues[playerInput] == playerO || boardValues[playerInput] == playerX )
 			{
 				cout << "Sorry already taken, choose again." << endl;
 				cin >> playerInput;
-				boardUpdate(playerTurn, playerInput, boardValues);
-				isSuccessPlayerO(); //stopped here, calling and testing isSuccessPlayerO() in main code
-				playerTurn = 2;
 			}
-			else
-			{
 				boardUpdate(playerTurn, playerInput, boardValues);
+				boardDisplay(boardValues);
+				isSuccessPlayerO(pSuccess, boardValues, playerO);
+				/*isTie();*/
 				playerTurn = 2;
-			}
 		}
 		else
 		{
 			cout << "Player 2, choose a spot to mark: " << endl;
 			cin >> playerInput;
-
-			if (boardValues[playerInput] == playerX)
+			while (playerInput > 8)
+			{
+				cout << "Invalid input. Pick a number between 0 and 8" << endl;
+				cin >> playerInput;
+			}
+			while (boardValues[playerInput] == playerO || boardValues[playerInput] == playerX)
 			{
 				cout << "Sorry already taken, choose again." << endl;
 				cin >> playerInput;
-				boardUpdate(playerTurn, playerInput, boardValues);
-				playerTurn = 1;
 			}
-			else
-			{
 				boardUpdate(playerTurn, playerInput, boardValues);
+				boardDisplay(boardValues);
+				isSuccessPlayerX(pSuccess, boardValues, playerX);
+				/*isTie();*/
 				playerTurn = 1;
-			}
 		}
-		//winConUpdate()
 	}
-	
 
 }
 
@@ -153,9 +139,19 @@ array<char, 9> boardUpdate(int playerTurn, int playerInput, array<char, 9> &boar
 		return boardValues;
 	}
 } //boardUpdate raplacing values in the array ( value identified by player input passed as index), by "o" or "x")
-bool* isSuccessPlayerO(bool* pSuccess, array<array<char, 3>, 8> winCons, char playerO)
+bool isSuccessPlayerO(bool* pSuccess, array<char, 9> boardValues, char playerO)
 {
-
+	array<array<char, 3>, 8> winCons =
+	{ {
+	{ boardValues[0], boardValues[1], boardValues[2] },
+	{ boardValues[0], boardValues[3], boardValues[6] },
+	{ boardValues[0], boardValues[4], boardValues[8] },
+	{ boardValues[1], boardValues[4], boardValues[7] },
+	{ boardValues[3], boardValues[4], boardValues[5] },
+	{ boardValues[2], boardValues[4], boardValues[6] },
+	{ boardValues[2], boardValues[5], boardValues[8] },
+	{ boardValues[6], boardValues[7], boardValues[8] }
+	} };
 	int a = 0;
 	int i = 0;
 	int counter = 0;
@@ -163,14 +159,11 @@ bool* isSuccessPlayerO(bool* pSuccess, array<array<char, 3>, 8> winCons, char pl
 	for (a = 0; a < 8; a++)
 	{
 		counter = 0;
-		cout << "a= " << a << endl;
 		for (i = 0; i < 3; i++)
 		{
-			cout << "char = " << winCons[a][i] << endl;
 			if (winCons[a][i] == playerO)
 			{
 				counter++;
-				cout << "counter = " << counter << endl;
 			}
 			else
 			{
@@ -178,7 +171,7 @@ bool* isSuccessPlayerO(bool* pSuccess, array<array<char, 3>, 8> winCons, char pl
 		}
 		if (counter == 3)
 		{
-			cout << "Victory!" << endl;
+			cout << "Victory! Congratulations Player 1" << endl;
 			*pSuccess = true;
 			break;
 		}
@@ -187,5 +180,68 @@ bool* isSuccessPlayerO(bool* pSuccess, array<array<char, 3>, 8> winCons, char pl
 	{
 		cout << "no victory conditions met" << endl;
 	}
-	return pSuccess;
+	return *pSuccess;
+}
+bool isSuccessPlayerX(bool* pSuccess, array<char, 9> boardValues, char playerX)
+{
+	array<array<char, 3>, 8> winCons =
+	{ {
+	{ boardValues[0], boardValues[1], boardValues[2] },
+	{ boardValues[0], boardValues[3], boardValues[6] },
+	{ boardValues[0], boardValues[4], boardValues[8] },
+	{ boardValues[1], boardValues[4], boardValues[7] },
+	{ boardValues[3], boardValues[4], boardValues[5] },
+	{ boardValues[2], boardValues[4], boardValues[6] },
+	{ boardValues[2], boardValues[5], boardValues[8] },
+	{ boardValues[6], boardValues[7], boardValues[8] }
+	} };
+	int a = 0;
+	int i = 0;
+	int counter = 0;
+
+	for (a = 0; a < 8; a++)
+	{
+		counter = 0;
+		for (i = 0; i < 3; i++)
+		{
+			if (winCons[a][i] == playerX)
+			{
+				counter++;
+			}
+			else
+			{
+			}
+		}
+		if (counter == 3)
+		{
+			cout << "Victory! Congratulations Player 2!" << endl;
+			*pSuccess = true;
+			break;
+		}
+	}
+	if (!*pSuccess)
+	{
+		cout << "no victory conditions met" << endl;
+	}
+	return *pSuccess;
+}
+bool isTie(array<char, 9> boardValues, char playerO, char playerX, bool* pTie)
+{
+	int counter = 0;
+	for (int i = 0; 1 < 9; i++)
+	{
+		if (boardValues[i] = playerO || playerX)
+		{
+			counter++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	if (counter == 9)
+	{
+		*pTie = true;
+		return *pTie;
+	}
 }
